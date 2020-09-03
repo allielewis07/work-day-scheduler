8a819d385bc4789cc77111943aae8bf61ec97809
allie Lewis <allielewis07@gmail.com>
2020-09-03 08:36:36 -0400

    starting project

---
+++ script.js
@@ -0,0 +1,8 @@
+//set moment to a variable
+var currentDate = moment().format("MMM Do YY");      
+// Make a variable for current date 
+
+// Set date 
+    $("#currentDay").text("Today's Date: " + currentDate);
+    
+// For loop to create rows with timeblocks, taskblocks, and save buttons
